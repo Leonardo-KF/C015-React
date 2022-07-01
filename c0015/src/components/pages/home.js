0f88f3279479d5285520cce445dbada8190b0a45
Leonardo Keiser Fleck <89053412+Leonardo-KF@users.noreply.github.com>
2022-06-30 21:30:37 -0300

    update useEffect

--- c0015/src/components/pages/home.js
+++ c0015/src/components/pages/home.js
@@ -1,18 +1,31 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { Card } from "../structure/card";
 import { GetAllBills } from "../../mocks/bills";
 
 export function Home() {
-  const [bills, setBills] = useState(GetAllBills);
+  const [bills, setBills] = useState([]);
+  const [envio, setEnvio] = useState();
 
   console.log(bills);
   console.log("mock: ", GetAllBills);
-  function handleSubmit(event) {
+
+  async function getBills() {
+    const req = await fetch("http://localhost:5000/bills");
+    const bills = await req.json();
+    setBills(bills);
+    setEnvio(false);
+  }
+
+  useEffect(() => {
+    getBills();
+  }, [envio]);
+
+  async function handleSubmit(event) {
     event.preventDefault();
+    // evento.alvo.nome.valor
 
     const newBill = {
-      id: parseInt(event.target.id.value),
-      title: event.target.title.value,
+      title: event.target.titulo.value,
       description: event.target.description.value,
       price: parseFloat(event.target.price.value),
       expirated: event.target.expirated.value,
@@ -32,14 +45,20 @@ export function Home() {
       }
     });
 
+    // UI Otimista
     if (!existId && !existTitle) {
-      setBills([...bills, newBill]);
+      setEnvio(true);
+      const req = await fetch("http://localhost:5000/bills", {
+        method: "POST",
+        body: JSON.stringify(newBill),
+        headers: new Headers({ "Content-Type": "application/json" }),
+      });
+      console.log(await req.json());
     }
   }
 
   return (
     <section className="page">
-      <h1>Passando nas Salas</h1>
       <div className="test">
         <form onSubmit={handleSubmit} className="form">
           <section className="section-inputs">
@@ -48,7 +67,7 @@ export function Home() {
           </section>
           <section className="section-inputs">
             <label>Title:</label>
-            <input name="title" type="text"></input>
+            <input name="titulo" type="text"></input>
           </section>
           <section className="section-inputs">
             <label>Price:</label>
@@ -62,7 +81,7 @@ export function Home() {
             <label>Expirated:</label>
             <input name="expirated" type="select"></input>
           </section>
-          <button type="submit">reset bills</button>
+          <button type="submit">Create Bill</button>
         </form>
       </div>
 
